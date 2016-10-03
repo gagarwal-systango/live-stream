f7f4811e1ad09570fd49a0c348347a7a127130ab
gagarwal-systango <gagarwal@isystango.com>
2016-10-03 18:59:02 +0530

    change in routes

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var ChannelFile = require('./../models/channelFile.js')
-
+var passport = require('passport');
+var User = require('./../models/user.js');
+var Video = require('./../models/video.js');
 var path = require('path');
 
 router.get('/', function(req, res, next){	
-  res.render('index');
+  var messages = req.flash('error');
+  console.log(messages);
+  res.render('signin', {msg: messages, user:req.user});
 });
 
 router.get('/allCh', function(req, res){
-  ChannelFile.find(function(err, files){
+  ChannelFile.find( {live: true}, function(err, files){
     if(err) throw err;
     res.render('allch', {ChannelFile: files});
   })
 });
-
-// router.use('/', isLoggedIn, function(req, res, next){
-//     next();
-// });
 
 router.get('/newch', isLoggedIn, function(req, res, next){	
   res.render('newch', {msg: ''});
@@ -40,7 +40,7 @@ router.post('/newch', function(req, res, next){
         channelFile.channelName = req.body.channelName;
         channelFile.totalChunks = 0;
         channelFile.token = req.body.token;
-        //channelFile.live = false
+        channelFile.live = true;
         channelFile.save(function(err, file){
           if(err) throw err;
           console.log("file: "+file.channelName);
@@ -58,7 +58,7 @@ router.get('/emitter', isLoggedIn, function(req, res, next){
 });
 
 router.get('/channel/:channelName', isLoggedIn, function (req, res) {
-  req.session.pubroom = req.params.channelName;
+  req.session.subroom = req.params.channelName;
   res.render('visualizer', {name: req.params.channelName});
 });
 
@@ -67,12 +67,52 @@ router.get('/hs', function(req, res, next){
 });
 
 
+router.get('/logout', isLoggedIn, function(req, res, next){
+    //removing session
+    req.session.destroy(function(err) {
+        if(err){
+          console.log(err);
+        }
+    });
+    //method added by passport
+    req.logout();    
+    console.log("logging out");
+    res.redirect('/');
+});
+
+//use notloggedin for all user routes written after it
+router.use('/', notLoggedIn, function(req, res, next){
+    next();
+});
+
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/newch',
+    failureRedirect: '/',
+    failureFlash: true
+}));
+
+router.post('/signin', passport.authenticate('local.signin', {
+  successRedirect: '/newch',
+  failureRedirect: '/',
+  failureFlash: true
+}));
+
 module.exports = router;
 	
-
+//passport method for logging in and logiing out
+//middelware
+//check for logged in
 function isLoggedIn(req, res, next){
   if (req.isAuthenticated()){
     return next();
   };
-  res.redirect('/user/signin');
+  res.redirect('/');
+}
+
+//check for not logged in
+function notLoggedIn(req, res, next){
+  if (!req.isAuthenticated()){
+    return next();
+  };
+  res.redirect('/');
 }
