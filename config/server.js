ba7499f73c704bacd387e2c01c02829b313d8dda
gagarwal-systango <gagarwal@isystango.com>
2016-10-05 13:36:11 +0530

    adding mlab uri in server

--- config/server.js
+++ config/server.js
@@ -1,5 +1,6 @@
 var  mongoose = require('mongoose');
-var url = "mongodb://localhost:27017/canvas";
+//var url = "mongodb://localhost:27017/canvas";
+var url = "mongodb://admin:admin@ds049496.mlab.com:49496/canvas";
 
 //connect to the mongod server
 mongoose.connect(url);
