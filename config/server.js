043022ec33fb4e49827eb169591671f1163bdf2c
gagarwal-systango <gagarwal@isystango.com>
2016-10-06 17:05:04 +0530

    adding buffer in client side

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 // connection with Mongo db.
 var mongoose = require('mongoose');
 
-var url = "mongodb://systango:systango1@ds047325.mlab.com:47325/livestreaming"
+var url = "mongodb://systango:systango1@ds047325.mlab.com:47325/livestreaming";
 
 //connect to the mongod server
 mongoose.connect(url);
--- public/js/visualizer.js
+++ public/js/visualizer.js
@@ -1,18 +1,36 @@
 var socket = io();
 var img = document.getElementById("play");
-var chunk = [];
+var chunk1 = [];
+var chunk2 = [];
+var img;
 
 socket.on('connect', function() {
     socket.emit('add subscriber', prompt("Enter token"));
 });
 
 socket.on('stream', function(image){
-    chunk.push(image);
-    if(chunk.length>20){
-        var img = document.getElementById("play");
-        img.src = chunk.shift();    
-    }
+    chunk2.push(image);
 });
+
+dispImages(0);
+
+function dispImages(i){
+    var s = setTimeout(function () {   
+	    if(chunk1.length==100 && i<=100){
+            img = document.getElementById("play");
+            img.src = chunk1[i];
+	        i++;
+    	    dispImages(i);
+	    }  
+        else if(chunk2.length<100){
+            dispImages(i);
+        }
+        else {
+            chunk1 = chunk2.splice(0,100);
+            dispImages(0);
+        }
+    }, 100);
+}
 
 socket.on('tokenError', function(data) {
     alert(data);
