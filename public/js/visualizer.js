05c71cf00341ab6cdaa5033633aa024e6c2ec4f1
gagarwal-systango <gagarwal@isystango.com>
2016-10-06 11:06:57 +0530

    adding chunk array to save img

--- public/js/visualizer.js
+++ public/js/visualizer.js
@@ -1,14 +1,17 @@
 var socket = io();
 var img = document.getElementById("play");
+var chunk = [];
 
 socket.on('connect', function() {
     socket.emit('add subscriber', prompt("Enter token"));
 });
 
-socket.on('stream', function(image) {
-    // get data from server.
-    var img = document.getElementById("play");
-    img.src = image;
+socket.on('stream', function(image){
+    chunk.push(image);
+    if(chunk.length>20){
+        var img = document.getElementById("play");
+        img.src = chunk.shift();    
+    }
 });
 
 socket.on('tokenError', function(data) {
