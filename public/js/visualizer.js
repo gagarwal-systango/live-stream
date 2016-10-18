718fb4c584ccb55b1b0729aef58ca5b03d68d1b5
varun sharma <varunsharma171994@gmail.com.com>
2016-10-18 12:35:05 +0530

    quality change

--- public/js/visualizer.js
+++ public/js/visualizer.js
@@ -9,21 +9,45 @@ socket.on('connect', function() {
     socket.emit('add subscriber', prompt("Enter token"));
 });
 
-socket.on('stream', function(data) {
-    console.log('before'+data);
-    var imgData = pako.inflate(data, { to: 'string' });
-   console.log('after'+imgData);
-     
-    img.src = imgData;
+socket.on('stream', function(image){
+    
+   var imgData = pako.inflate(image, { to: 'string' });
+   chunk2.push(imgData);
 });
+
+dispImages(0);
+
+function dispImages(i){
+    var s = setTimeout(function () {   
+	    if(chunk1.length>=5 && i<5){
+            
+            img.src = chunk1[i];
+	        i++;
+    	    dispImages(i);
+	    }  
+        else if(chunk1.length==5 && chunk2.length<5){
+            img.src = chunk1[4];
+            dispImages(i);
+        }
+        else if(chunk2.length<5){
+            dispImages(i);
+        }
+        else {
+            chunk1 = chunk2.splice(0,5);
+            dispImages(0);
+        }
+    }, 100);
+}
+
 socket.on('stream1', function(audioData) {
    
      if (audioData !== null && audioData !== undefined) {
         var audioInfo = audioData;
         console.log("id:=" + audioInfo.id + "Position:= " + audioInfo.playingPosition);
     }
-    
 });
+
+
 socket.on('tokenError', function(data) {
     alert(data);
     socket.emit('add subscriber', prompt("Enter token"));
